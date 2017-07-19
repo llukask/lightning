b942b7172936c0d7cc5d97336f2221f42095f89e
Lukas Kilian <kilian@dolph.in>
2017-07-19 09:15:02 +0200

    Initial commit

---
+++ src/main/java/tech/beeflord/blah/BlahApplication.java
@@ -0,0 +1,12 @@
+package tech.beeflord.blah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlahApplication.class, args);
+	}
+}
